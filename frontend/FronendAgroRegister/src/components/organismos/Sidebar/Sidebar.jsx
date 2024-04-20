22582debda7e97a17d048af2f9a5409b909afc97
estebangaviria2006 <munozleandro343@gmail.com>
2024-04-19 23:48:29 -0500

    Añado cerrar sesion

--- frontend/FronendAgroRegister/src/components/organismos/Sidebar/Sidebar.jsx
+++ frontend/FronendAgroRegister/src/components/organismos/Sidebar/Sidebar.jsx
@@ -11,10 +11,9 @@ function Sidebar() {
           <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div className="offcanvas-body">
-          <img className='imagenpersonal' src={v.Imagepersona} alt="Imagen 2" style={{ width: '100px', objectFit: 'cover', height: '100px' }} />
-          <h1 style={{ textAlign: 'center', fontSize: '29px', paddingTop: '20px' }}>Sergio C</h1>
+          <img className='imagenpersonal' src={v.imageLogo} alt="Imagen 2" style={{ width: '100px', objectFit: 'cover', height: '100px' }} />
           <ul className="navbar-nav ">
-            {/* <NavItem icon={v.iconoPerfilUsuario} text="Perfil" href="/Perfilprincipal" /> */}
+           {/*  <NavItem icon={v.iconoPerfilUsuario} text="Perfil" href="/Perfilprincipal" />  */}
             <NavItem icon={v.iconoFinca} text="Finca" href="/finca" />
             <NavItem icon={v.iconoCultivo} text="Cultivos" href="/cultivo" />
             <NavItem icon={v.iconoTractor} text="Lotes" href="/lotes" />
@@ -22,8 +21,8 @@ function Sidebar() {
             <NavItem icon={v.iconoActividad} text="Actividad" href="/actividad" />
             <NavItem icon={v.iconoTractor} text="Recursos" href="/recursos" />
             <NavItem icon={v.iconoReporte} text="Reportes" href="/reportes" />
-            {/*  <NavItem icon={v.iconoSoporte} text="Soporte" href="/soport" /> */}
-            {/* <NavItem icon={v.iconoSalir} text="Salir" href="/" /> */}
+           {/*   <NavItem icon={v.iconoSoporte} text="Soporte" href="/soport" /> */}
+            {/* <NavItem icon={v.iconoSalir} text="Salir" href="/" />  */}
           </ul>
 
         </div>
