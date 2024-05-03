4f6e470d3fc8c74256a91324d9c7fd4e286c8dc7
sergio3629 <sergiocanacue0@gmail.com>
2024-05-03 07:04:49 -0500

    ruta usuarios

--- backend/src/routes/routes.usuarios.js
+++ backend/src/routes/routes.usuarios.js
@@ -2,15 +2,15 @@ import { Router } from "express"
 import  {actualizarUsuario, listarUsuarios, buscarUsuario, desactivarUsuario, registrarUsuarios} from '../controllers/controller.usuarios.js'
 
 import {validarUsuario, validarUsu} from '../../validate/UsuariosValidatekvd.js'
-import { validarToken } from '../controllers/autenticacion.js'
+//import { validarToken } from '../controllers/autenticacion.js'
 
 const rutaUsuario = Router();
 
-rutaUsuario.get('/listarUsuario', validarToken,listarUsuarios);  
+rutaUsuario.get('/listarUsuario',listarUsuarios);  
 rutaUsuario.post('/registrarUsuario',validarUsuario,registrarUsuarios);
-rutaUsuario.post('/desactivarUsuario/:id_usuario',validarToken, desactivarUsuario);
-rutaUsuario.put('/actualizarUsuario/:id_usuario',validarToken, validarUsu,actualizarUsuario);
-rutaUsuario.get('/buscarUsuarios/:id_usuario',validarToken, buscarUsuario);
+rutaUsuario.post('/desactivarUsuario/:id_usuario', desactivarUsuario);
+rutaUsuario.put('/actualizarUsuario/:id_usuario', validarUsu,actualizarUsuario);
+rutaUsuario.get('/buscarUsuarios/:id_usuario', buscarUsuario);
 
 
 export default rutaUsuario;
