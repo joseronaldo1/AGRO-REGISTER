58cafe4f025bb8c31b36deb59b19ddb00cf8eb4b
estebangaviria2006 <munozleandro343@gmail.com>
2024-05-22 11:20:14 -0500

    add controlador produccion

--- backend/validate/ProduccionValidateDevpap.js
+++ backend/validate/ProduccionValidateDevpap.js
@@ -4,7 +4,7 @@ import { check } from "express-validator";
 export const ValidateProduccion = [
     check('cantidad_produccion', 'La cantidad debe ser un número y no ser menor a 1').isInt().isNumeric(),
     check('precio', 'El precio debe ser un número y mayor a 1').isInt().isNumeric(),
-    check('fk_id_programacion').notEmpty().withMessage().isNumeric().withMessage(),
+
 
 
 ];
