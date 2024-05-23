31d900cd7bce1d3a4a271ace6b7628069c233740
Pabloandres4112 <perdomo4112@gmail.com>
2024-05-22 23:25:58 -0500

    hol

--- frontend/FronendAgroRegister/src/App.jsx
+++ frontend/FronendAgroRegister/src/App.jsx
@@ -22,36 +22,95 @@ import { ReportesPage } from './pages';
 import { ProgramacionPage } from './pages';
 import { ProduccionPage } from './pages';
 import { PerfilprincipalPage } from './pages';
+import ProtectedRoute from './pages/ProteccionRuta';
+import { AuthProvider } from './pages/AuthContext';
 
 
 
 function App() {
   return (
+    <AuthProvider>
     <BrowserRouter>
-
-
       <Routes>
         <Route path="/" element={<InicioSesionPage />} />
-        <Route path="/Registrarse" element={<RegistroPage />} />
-        <Route path="/perfil" element={<EditarPerfilUsuarioPage />} />
+        <Route path="/registrarse" element={<RegistroPage />} />
+        <Route path="/login" element={<InicioSesionPage />} /> {/* Agrega la ruta de login */}
         <Route path="/olvidocontra1" element={<OlvidopassonePage />} />
         <Route path="/olvidocontra2" element={<OlvidopasstwoPage />} />
         <Route path="/olvidocontra3" element={<OlvidopasstreePage />} />
-        <Route path="/finca" element={<FincaPage />} />
-        <Route path="/usuarios" element={<UsuarioPage />} />
-        <Route path="/variedad" element={<VariedadPage />} />
-        <Route path="/cultivo" element={<CultivosPage />} />
-        <Route path="/actividad" element={<ActividadPage />} />
-        <Route path="/lotes" element={<LotesPage />} />
-        <Route path="/recursos" element={<RecursosPage />} />
-        <Route path="/Soporte" element={<SoportePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/reportes" element={<ReportesPage />} />
-        <Route path="/programacion" element={<ProgramacionPage />} />
-        <Route path="/produccion" element={<ProduccionPage />} />
-        <Route path="/Perfilprincipal" element={<PerfilprincipalPage />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/perfil" element={
+          <ProtectedRoute>
+            <EditarPerfilUsuarioPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/finca" element={
+          <ProtectedRoute>
+            <FincaPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/usuarios" element={
+          <ProtectedRoute>
+            <UsuarioPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/variedad" element={
+          <ProtectedRoute>
+            <VariedadPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/cultivo" element={
+          <ProtectedRoute>
+            <CultivosPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/actividad" element={
+          <ProtectedRoute>
+            <ActividadPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/lotes" element={
+          <ProtectedRoute>
+            <LotesPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/recursos" element={
+          <ProtectedRoute>
+            <RecursosPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/soporte" element={
+          <ProtectedRoute>
+            <SoportePage />
+          </ProtectedRoute>
+        } />
+        <Route path="/reportes" element={
+          <ProtectedRoute>
+            <ReportesPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/programacion" element={
+          <ProtectedRoute>
+            <ProgramacionPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/produccion" element={
+          <ProtectedRoute>
+            <ProduccionPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/perfilprincipal" element={
+          <ProtectedRoute>
+            <PerfilprincipalPage />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
+    </AuthProvider>
   );
 }
 
